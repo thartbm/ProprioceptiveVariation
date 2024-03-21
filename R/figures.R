faf9d3ca8ccad0f0456dffba7a7bca8ec3139bac
Marius 't Hart <thartbm@gmail.com>
2024-03-21 16:33:18 -0400

    added function to plot central fits, and function to plot all bootstrapped parameter confidence intervals

--- R/figures.R
+++ R/figures.R
@@ -108,3 +108,134 @@ plotData <- function() {
           bty='n')
   
 }
+
+
+plotCentralFits <- function() {
+  
+  layout(mat=matrix(c(1:2),ncol=1,nrow=2))
+  
+  df <- read.csv('data/centralfits.csv', stringsAsFactors = FALSE)
+  
+  for (task in c('localization','training')) {
+  
+    plotParadigm(xticks='block',ylab='angular deviation [°]')
+    title(main=task)
+    
+    fdf <- df[which(df$type == task),]
+    
+    # plot the actual data:
+    tdf <- read.csv(sprintf('data/%s.csv',task), stringsAsFactors = FALSE)
+    
+    tdf$depvar <- tdf[,list('localization'='taperror_deg', 'training'='reachdeviation_deg')[[task]]]
+    
+    mf <- 1
+    if (task=='training') {mf <- -1}
+    tdf$depvar <- mf*tdf$depvar
+    
+    FUN=mean
+    avg <- aggregate(depvar ~ pair, data=tdf, FUN=FUN, na.rm=TRUE)
+    CI  <- aggregate(depvar ~ pair, data=tdf, FUN=function(d) Reach::getConfidenceInterval(d, method='t', FUN=FUN))
+    
+    color <- list('localization'='red', 'training'='blue')[[task]] 
+    
+    polygon(
+      x = c(CI$pair, rev(CI$pair)),
+      y = c(CI$depvar[,1], rev(CI$depvar[,2])),
+      border=NA,
+      col=Reach::colorAlpha(color, alpha=30)
+    )
+    lines( x = avg$pair,
+           y = avg$depvar,
+           col=Reach::colorAlpha(color, alpha=80) )
+    
+    
+    trial <- 50
+    for (block_idx in c(1:dim(fdf)[1])) {
+      bpar <- c( 'lambda' = fdf$lambda[block_idx],
+                 'N0'     = fdf$N0[block_idx]      )
+
+      btrials <- fdf$trials[block_idx]
+
+      expfit <- Reach::exponentialModel(par=bpar, timepoints=btrials)$output
+      expfit <- (expfit * fdf$mf[block_idx] * fdf$asymptote[block_idx]) + fdf$startpoint[block_idx]
+
+      lines(x=c(1:length(expfit))+trial,
+            y=mf*expfit,
+            col=color)
+
+
+      trial <- trial + btrials
+    }
+    
+    
+  }
+
+}
+
+
+plotBootstrappedParameters <- function(task) {
+  
+  mf <- 1
+  if (task == 'training') { mf <- -1 }
+  
+  color <- list('localization'='red', 'training'='blue')[[task]] 
+  
+  tdf <- read.csv(sprintf('data/%s_bootstraps.csv', task), stringsAsFactors = FALSE)
+  
+  cfdf <- read.csv('data/centralfits.csv', stringsAsFactors = FALSE)
+  fdf <- cfdf[which(cfdf$type == task),]
+  
+  lambda_CIs <- aggregate(lambda ~ block, data=tdf, FUN=quantile, prob=c(0.025, 0.975))
+  N0_CIs     <- aggregate(N0     ~ block, data=tdf, FUN=quantile, prob=c(0.025, 0.975))
+  
+  layout(mat=matrix(c(1:2),nrow=2,ncol=1))
+  
+  
+  
+  lambda_range <- range(c(range(lambda_CIs$lambda[,1]), range(lambda_CIs$lambda[,2])))
+  
+  plot(-1000, -1000,
+       main='learning rate', xlab='', ylab='',
+       xlim=c(0,31), ylim=c(0,1),
+       ax=F, bty='n')
+  
+  for (b.idx in c(1:30)) {
+    lo <- lambda_CIs$lambda[b.idx,1]
+    hi <- lambda_CIs$lambda[b.idx,2]
+    polygon( x = c(-0.4, 0.4, 0.4, -0.4)+b.idx,
+             y = c(lo, lo, hi, hi),
+             col = Reach::colorAlpha(color),
+             border=NA)
+    points( x=b.idx,
+            y=fdf$lambda[which(fdf$block == b.idx)],
+            col=color)
+  }
+  
+  axis(side=1, at=c(1:30), cex.axis=0.8, las=2)
+  axis(side=2, at=c(0,1))
+  
+  
+  N0_range <- range(c(range(N0_CIs$N0[,1]), range(N0_CIs$N0[,2])))
+  
+  plot(-1000, -1000,
+       main='asymptote', xlab='', ylab='',
+       xlim=c(0,31), ylim=N0_range,
+       ax=F, bty='n')
+  
+  for (b.idx in c(1:30)) {
+    lo <- N0_CIs$N0[b.idx,1]
+    hi <- N0_CIs$N0[b.idx,2]
+    polygon( x = c(-0.4, 0.4, 0.4, -0.4)+b.idx,
+             y = c(lo, lo, hi, hi),
+             col = Reach::colorAlpha(color),
+             border=NA)
+    points( x=b.idx,
+            y=fdf$N0[which(fdf$block == b.idx)],
+            col=color)
+  }
+  
+  
+  axis(side=1, at=c(1:30), cex.axis=0.8, las=2)
+  axis(side=2, at=round(N0_range, digits=2))
+  
+}
